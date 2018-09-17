a5dde51a32791fc3441b692ab833fe71606312fa
swkane <samuelwkane@gmail.com>
2018-09-17 10:34:45 -0400

    header buttons

--- App.js
+++ App.js
@@ -14,13 +14,34 @@ class LogoTitle extends React.Component {
 }
 
 class HomeScreen extends React.Component {
-  static navigationOptions = {
-    headerTitle: <LogoTitle />
+  static navigationOptions = ({ navigation }) => {
+    return {
+      headerTitle: <LogoTitle />,
+      headerRight: (
+        <Button
+          onPress={navigation.getParam("increaseCount")}
+          title="+1"
+          color="#fff"
+        />
+      )
+    };
   };
+
+  componentDidMount() {
+    this.props.navigation.setParams({ increaseCount: this._increaseCount });
+  }
+
+  state = { count: 0 };
+
+  _increaseCount = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
   render() {
     return (
       <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
         <Text>Home Screen</Text>
+        <Text>{this.state.count}</Text>
         <Button
           title="Go to Details"
           onPress={() =>
